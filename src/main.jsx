0de5ca630a96d93743b15a372cb9e49439e3b555
afifarioss <afif.peugeot@gmail.com>
2026-06-05 10:17:34 +0800

    Update main.jsx
    Fix src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TradeTalk from './TradeTalk.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TradeTalk />
+  </React.StrictMode>,
+)
