cdb5c30bbf454fa8bb07b9c637d12706c4a28ee7
afifarioss <afif.peugeot@gmail.com>
2026-06-05 11:28:06 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { WagmiProvider, createConfig, http } from 'wagmi'
+import { base } from 'wagmi/chains'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import TradeTalk from './TradeTalk.jsx'
+
+const config = createConfig({
+  chains: [base],
+  transports: {
+    [base.id]: http(),
+  },
+})
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TradeTalk />
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <TradeTalk />
+      </QueryClientProvider>
+    </WagmiProvider>
   </React.StrictMode>,
 )
